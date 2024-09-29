7be5e2eb0c78bf60684647664753a5ad662c91c5
Francisco LR <francisco.lopez247037@potros.itson.edu.mx>
2024-09-28 20:43:03 -0700

    clase usuario

--- src/main/java/itson/patrond/usuario/usuario.java
+++ src/main/java/itson/patrond/usuario/usuario.java
@@ -4,10 +4,76 @@
  */
 package itson.patrond.usuario;
 
+import itson.patrond.rutina.PlanEntrenamiento;
+
 /**
- *
+ * Clase que representa un usuario
  * @author Fran
  */
-public class usuario {
+public class Usuario {
+    String nombre;
+    String nomUsuario;
+    int edad;
+    float peso;
+    float estatura;
+    PlanEntrenamiento  plan;
+
+    public Usuario(String nombre, String nomUsuario, int edad, float peso, float estatura, PlanEntrenamiento plan) {
+        this.nombre = nombre;
+        this.nomUsuario = nomUsuario;
+        this.edad = edad;
+        this.peso = peso;
+        this.estatura = estatura;
+        this.plan = plan;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNomUsuario() {
+        return nomUsuario;
+    }
+
+    public void setNomUsuario(String nomUsuario) {
+        this.nomUsuario = nomUsuario;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(float estatura) {
+        this.estatura = estatura;
+    }
+
+    public PlanEntrenamiento getPlan() {
+        return plan;
+    }
+
+    public void setPlan(PlanEntrenamiento plan) {
+        this.plan = plan;
+    }
+
     
 }
